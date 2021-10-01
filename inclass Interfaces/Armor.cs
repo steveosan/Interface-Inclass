a32b575c09d82d26c0fccca19e6e2162f0151eeb
steveosan <70219345+steveosan@users.noreply.github.com>
2021-09-30 20:17:54 -0500

    Add files via upload

---
+++ inclass Interfaces/Armor.cs
@@ -0,0 +1,15 @@
+namespace inclass_Interfaces
+{
+    public class Armor
+    {
+        public string Name {get;set;}
+
+        public int Defence {get;set;}
+
+        public Armor()
+        {
+            Name = "armor";
+
+        }
+    }
+}
---
+++ inclass Interfaces/IMonster.cs
@@ -0,0 +1,11 @@
+namespace inclass_Interfaces
+{
+    public interface IMonster : ICombatant
+    {
+
+
+        void Roar() 
+        {
+        }
+    }
+}
